3b26c794b8ad12807e51537da07e14a2ef663f26
Maeve Murphy Quinlan <earmmu@leeds.ac.uk>
2025-07-04 16:45:34 +0100

    Update notebooks

--- src/safer_parks/bounds.py
+++ src/safer_parks/bounds.py
@@ -3,12 +3,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 import osmnx as ox
 
+
+# This is a very basic boundary calculation
 def basic_boundary(location = "Peel Park, Bradford, UK"):
     tags = {"leisure": "park"}
     parks = ox.features.features_from_address(location, tags, 200)
     park = parks.iloc[0]
     park_polygon = park.geometry  # unbuffered!
     return park_polygon
+
+# more complex park boundary calculation
+
+def park_greenspace_boundary():
+    pass
 
 def buffer_network(park_polygon, buffer_m=0.001, type="walk"):
     network_buffer = park_polygon.buffer(buffer_m)
